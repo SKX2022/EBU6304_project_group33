b0bd6d90230444e2a8d17ba00f0f56315ec432f2
Yuqi Wang <166500000+ddtzalsm@users.noreply.github.com>
2025-05-22 20:07:20 +0800

    完善了Figure界面的UI

--- src/com/finance/controller/GraphController.java
+++ src/com/finance/controller/GraphController.java
@@ -1,4 +1,14 @@
 package com.finance.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class GraphController {
+    public void analyzeAndSuggest(ActionEvent actionEvent) {
+
+    }
+
+    public void handleDateRangeSelection(MouseEvent mouseEvent) {
+
+    }
 }
